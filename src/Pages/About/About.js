d8419f0c257350adb2c5bd04e47d97a850625e56
muradxkira <mahfujur797@gmail.com>
2022-12-21 10:28:05 +0600

    projects and home page image added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,12 +6,14 @@ const About = () => {
             <h1 className='text-4xl font-bold py-5'>⚡ About Me</h1>
             <p className='w-full lg:w-1/2 text-gray-300/80'>
                 Hey! I'm MD Mahfujur Rahman, I've been close to a computer since an early age, and been passionate about it ever since.
-
+                <br />
+                <br />
                 I really liked to build stuff using no-code tools
                 back in 2016, and from that, I explored how to code myself, fast-forward to today, I do programming in various languages and technologies.
                 I'm interested in building something awesome with code and automate tasks with code, currently focused on Web Development,
-
-                <span className='pt-5'>
+                <br />
+                <br />
+                <span>
                     When I'm not coding I play games with my friends, watch some show, or if the weather's good then go out for a walk!
                 </span>
             </p>
