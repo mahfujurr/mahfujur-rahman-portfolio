94da617417ae7ccc3ab8e9b510898f00b7833c31
muradxkira <mahfujur797@gmail.com>
2023-01-06 21:11:21 +0600

    about updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,21 +2,26 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className=' px-5 md:px-24 lg:px-48 pb-96 pt-24'>
-            <h1 className='text-4xl font-bold py-5'>⚡ About Me</h1>
-            <p className='w-full lg:w-1/2 text-gray-300/80'>
-                Hey! I'm MD Mahfujur Rahman, I've been close to a computer since an early age, and been passionate about it ever since.
-                <br />
-                <br />
-                I really liked to build stuff using no-code tools
-                back in 2016, and from that, I explored how to code myself, fast-forward to today, I do programming in various languages and technologies.
-                I'm interested in building something awesome with code and automate tasks with code, currently focused on Web Development,
-                <br />
-                <br />
-                <span>
-                    When I'm not coding I play games with my friends, watch some show, or if the weather's good then go out for a walk!
-                </span>
-            </p>
+        <div className=' px-5 md:px-24 lg:px-24 pb-96 pt-24'>
+            <div>
+                <h1 className='text-4xl font-bold py-5'>⚡ About Me</h1>
+                <p className='w-full lg:w-1/2 text-gray-300/80'>
+                    Hey! I'm MD Mahfujur Rahman, I've been close to a computer since an early age, and been passionate about it ever since.
+                    <br />
+                    <br />
+                    I really liked to build stuff using no-code tools
+                    back in 2016, and from that, I explored how to code myself, fast-forward to today, I do programming in various languages and technologies.
+                    I'm interested in building something awesome with code and automate tasks with code, currently focused on Web Development,
+                    <br />
+                    <br />
+                    <span>
+                        When I'm not coding I play games with my friends, watch some show, or if the weather's good then go out for a walk!
+                    </span>
+                </p>
+            </div>
+            <div>
+                <h1>Age</h1>
+            </div>
         </div>
     );
 };
